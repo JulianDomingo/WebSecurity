27fd7b7069a7d7b2b23618c97f64c93dcdc1aefa
Julian Domingo <julian.domingo@utexas.edu>
2017-02-16 22:09:48 -0600

    Added starter code for assignment 4

--- project1/globitek/private/db_credentials.php
+++ project1/globitek/private/db_credentials.php
@@ -1,6 +1,6 @@
 <?php
   define("DB_SERVER", "localhost");
   define("DB_USER", "root");
-  define("DB_PASS", "<My private password. Local repo has actual password.>");
+  define("DB_PASS", "Qud54gd54!");
   define("DB_NAME", "globitek");
 ?>
